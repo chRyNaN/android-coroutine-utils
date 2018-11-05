bb1f87c6e49ee416a84e4094d9a947dbcf87cb3b
Chris <ckeenan@starry.com>
2018-11-05 11:11:09 -0500

    Added more coroutine lifecycles

--- lifecycle/src/main/java/com/chrynan/aclifecycle/AndroidCoroutineLifecycleUtils.kt
+++ lifecycle/src/main/java/com/chrynan/aclifecycle/AndroidCoroutineLifecycleUtils.kt
@@ -1,7 +1,12 @@
-@file:Suppress("MemberVisibilityCanBePrivate", "unused")
+@file:Suppress("MemberVisibilityCanBePrivate", "unused", "DEPRECATION")
 
 package com.chrynan.aclifecycle
 
+import android.app.Activity
+import android.app.Fragment
+import android.app.Service
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.OnLifecycleEvent
@@ -10,10 +15,18 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlin.coroutines.CoroutineContext
 
-abstract class CoroutineLifecycleObservers : CoroutineScope,
+interface BaseCoroutineScope : CoroutineScope {
+
+    override val coroutineContext: CoroutineContext
+        get() = UI + job
+
+    val job: Job
+}
+
+abstract class CoroutineLifecycleObserver : BaseCoroutineScope,
     LifecycleObserver {
 
-    protected lateinit var job: Job
+    override lateinit var job: Job
 
     override val coroutineContext: CoroutineContext
         get() = UI + job
@@ -29,10 +42,82 @@ abstract class CoroutineLifecycleObservers : CoroutineScope,
     }
 }
 
-interface BaseCoroutineScope : CoroutineScope {
+abstract class CoroutineActivity : Activity(),
+    BaseCoroutineScope {
 
-    override val coroutineContext: CoroutineContext
-        get() = UI + job
+    override lateinit var job: Job
 
-    val job: Job
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
+
+abstract class CoroutineAppCompatActivity : AppCompatActivity(),
+    BaseCoroutineScope {
+
+    override lateinit var job: Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
+
+abstract class CoroutineFragment : Fragment(),
+    BaseCoroutineScope {
+
+    override lateinit var job: Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
+
+abstract class CoroutineAppCompatFragment : android.support.v4.app.Fragment(),
+    BaseCoroutineScope {
+
+    override lateinit var job: Job
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
+}
+
+abstract class CoroutineService : Service(),
+    BaseCoroutineScope {
+
+    override lateinit var job: Job
+
+    override fun onCreate() {
+        super.onCreate()
+        job = Job()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
+    }
 }
